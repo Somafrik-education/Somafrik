f09647cc23c372adac64232be4d2cf78e000e686
Somafrik <somafrik@outlook.fr>
2026-08-18 00:26:36 +0200

    test(students): verify general canonical identity format

--- backend/lib/studentCanonicalIdentifier.test.js
+++ backend/lib/studentCanonicalIdentifier.test.js
@@ -7,25 +7,22 @@ const {
   isStudentCanonicalCode,
   parseStudentCanonicalCode,
   resolveSchoolIdentityContext,
+  studentIdentityInitials,
 } = require("./studentCanonicalIdentifier");
 
+assert.equal(studentIdentityInitials("OKITO", "Hope Sabrina"), "OHS");
 assert.equal(
-  formatStudentCanonicalCode({ countryCode: "cd", schoolInitials: "in", year: 2026, sequence: 1 }),
-  "CD-IN-EL-26-001",
+  formatStudentCanonicalCode({ countryCode: "cd", schoolInitials: "in", studentInitials: "ohs", year: 2026, sequence: 1 }),
+  "CD-IN-OHS-26-00001",
 );
-assert.equal(
-  formatStudentCanonicalCode({ countryCode: "CD", schoolInitials: "IN", year: 2026, sequence: 12 }),
-  "CD-IN-EL-26-012",
-);
-assert.equal(isStudentCanonicalCode("CD-IN-EL-26-001"), true);
+assert.equal(isStudentCanonicalCode("CD-IN-OHS-26-00001"), true);
+assert.equal(isStudentCanonicalCode("CD-IN-EL-26-001"), false);
 assert.equal(isStudentCanonicalCode("PENDING"), false);
-assert.equal(isStudentCanonicalCode("ELE-CD-0001-0001-000001"), false);
-assert.equal(isStudentCanonicalCode("ELE-0001"), false);
-assert.equal(isStudentCanonicalCode("CD-IN-JPK-26-00004"), false);
 
-assert.deepEqual(parseStudentCanonicalCode("cd-in-el-26-001"), {
+assert.deepEqual(parseStudentCanonicalCode("cd-in-ohs-26-00001"), {
   countryCode: "CD",
   schoolInitials: "IN",
+  studentInitials: "OHS",
   yearShort: "26",
   sequence: 1,
 });
@@ -34,36 +31,36 @@ assert.equal(
   generateNextStudentCanonicalCode({
     countryCode: "CD",
     schoolInitials: "IN",
+    firstName: "Hope Sabrina",
+    lastName: "OKITO",
     year: 2026,
     existingCodes: [],
   }),
-  "CD-IN-EL-26-001",
+  "CD-IN-OHS-26-00001",
 );
 assert.equal(
   generateNextStudentCanonicalCode({
     countryCode: "CD",
     schoolInitials: "IN",
+    firstName: "Jean Pierre",
+    lastName: "KABILA",
     year: 2026,
-    existingCodes: ["CD-IN-EL-26-001", "CD-IN-EL-26-003", "ELE-0001"],
+    existingCodes: ["CD-IN-OHS-26-00001", "CD-IN-ABC-26-00003"],
   }),
-  "CD-IN-EL-26-004",
-);
-assert.notEqual(
-  generateNextStudentCanonicalCode({ countryCode: "CD", schoolInitials: "IN", year: 2026, existingCodes: [] }),
-  generateNextStudentCanonicalCode({ countryCode: "BI", schoolInitials: "IN", year: 2026, existingCodes: [] }),
+  "CD-IN-KJP-26-00004",
 );
 
-assert.deepEqual(
-  resolveSchoolIdentityContext({ loginCode: "CD-IN-26-001" }),
-  { countryCode: "CD", schoolInitials: "IN" },
-);
+assert.deepEqual(resolveSchoolIdentityContext({ loginCode: "CD-IN-26-001" }), {
+  countryCode: "CD",
+  schoolInitials: "IN",
+});
 assert.deepEqual(
   resolveSchoolIdentityContext({ name: "Institut Nuru", school_code: "CD-2026-0001" }),
   { countryCode: "CD", schoolInitials: "IN" },
 );
 
 assert.throws(
-  () => formatStudentCanonicalCode({ countryCode: "CD", schoolInitials: "IN", year: 2026, sequence: 1000 }),
+  () => formatStudentCanonicalCode({ countryCode: "CD", schoolInitials: "IN", studentInitials: "OHS", year: 2026, sequence: 100000 }),
   (error) => error.code === "STUDENT_SEQUENCE_EXHAUSTED",
 );
 
